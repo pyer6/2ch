bbee90bc049d49b75fd6714d37b93e8d9a032d64
kazama <chipdale0119@gmail.com>
2021-09-15 15:50:23 +0900

    Create GuestUser

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,4 +3,5 @@ class Talk < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: {in: 1..255}
+  default_scope -> { order(created_at: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "top#index"
   devise_for :users, controllers: { registrations: 'users/registrations'}
+  devise_scope :user do
+    post "/users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :rooms, only: [:new, :create, :show, :destroy] do
     resources :talks, only: [:create, :destroy]
   end
